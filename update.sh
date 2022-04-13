635ed343099582a6b59cfbfdfbfb4c216a8dddc9
Marco Sadocco <69188164+Jamarkuskg@users.noreply.github.com>
2022-04-13 14:30:17 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
-sudo apt clean
+#update packages
 sudo apt update
+#upgrade packages to the last version and removes old packages
 sudo apt full-upgrade -y
+#check if there are some release upgrade
 sudo do-release-upgrade
+#remove all the dependencies that aren't used
 sudo apt autoremove -y
+#clean the cache
 sudo apt clean
-sudo apt purge
